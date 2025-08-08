9c9b61ab88b7ad48568d8be8784d80f83886f585
pk4727 <pradhumanpk2018@gmail.com>
2025-08-08 21:33:57 +0530

    First commit

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,12 @@
-console.log('pk');
-console.log('pkk');
-let x = 5;
-console.log(x);
+console.log("Welcome to JS"); // print in console
+
+// vriables
+let a = 5; // variable declaration
+a = 20; // variable reassignment
+let b = 10.0;
+let c = "Pk";
+console.log(a,b,c);
+
+const d = 50; // constant declaration
+console.log(d);
+
