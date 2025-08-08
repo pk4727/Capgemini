e0c916baa8165bd0d1e93d6051e7381355899c8d
pk4727 <pradhumanpk2018@gmail.com>
2025-08-08 22:01:20 +0530

    second commit

--- javaScript.js
+++ javaScript.js
@@ -5,8 +5,12 @@ let a = 5; // variable declaration
 a = 20; // variable reassignment
 let b = 10.0;
 let c = "Pk";
-console.log(a,b,c);
+console.log(a, b, c);
 
 const d = 50; // constant declaration
+// d = 60; // Uncommenting this line will cause an error because 'd' is a constant
 console.log(d);
 
+document.getElementById("b").addEventListener("click", function () {
+    alert("Paper clicked!");
+});
