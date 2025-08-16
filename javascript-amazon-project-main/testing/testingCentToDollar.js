230a698e89536002129e008346c6aac4b72f43b6
pk4727 <pradhumanpk2018@gmail.com>
2025-08-16 11:00:56 +0530

    adding result to its id

--- javascript-amazon-project-main/testing/testingCentToDollar.js
+++ javascript-amazon-project-main/testing/testingCentToDollar.js
@@ -3,20 +3,20 @@ import { centToDollar } from "../scripts/calculation.js";
 let result = document.querySelector(".testing-result");
 
 let centMoney = [
-    { cent: 0, dollar: '0.00' },
-    { cent: 2095, dollar: '20.95' },
-    { cent: 2000.5, dollar: '20.01' },
-    { cent: 2000.4, dollar: '20.00' }
+    { id: 'zero', cent: 0, dollar: '0.00' },
+    { id: 'exect', cent: 2095, dollar: '20.95' },
+    { id: 'celing', cent: 2000.5, dollar: '20.01' },
+    { id: 'floor', cent: 2000.4, dollar: '20.00' }
 ]
+
 
 let output = "";
 centMoney.forEach((testcase) => {
-    if (centToDollar(testcase.cent)==testcase.dollar){
-        output += "\nPass";
-    }else{
-        output += "\nFail";
+    if (centToDollar(testcase.cent) == testcase.dollar) {
+        output += `${testcase.id} : Pass <br> `;
+    } else {
+        output += `${testcase.id} : Fail <br> `;
         console.log(testcase.cent + " " + centToDollar(testcase.cent));
     }
 })
-
 result.innerHTML = output;
