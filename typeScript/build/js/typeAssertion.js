ade1765ebca946c952b9df258d5cfb9449f6c568
pk4727 <pradhumanpk2018@gmail.com>
2025-08-17 23:43:28 +0530

    dom updation, class, inheritance, interface, class with static member, getter & setter in class

--- typeScript/build/js/typeAssertion.js
+++ typeScript/build/js/typeAssertion.js
@@ -1,2 +1,17 @@
+// convert to more or less specific
+let a = 'hello';
+let b = a; // less specific
+let c = a; // more specific
+let d = 'hello';
+let e = 'world';
+const addConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
+};
+const add = addConcat(2, 3, 'add');
+const concat = addConcat(2, 3, 'concat'); // this is an error but ts not understand it
+// unknown | double casting
+10;
 export {};
 //# sourceMappingURL=typeAssertion.js.map
