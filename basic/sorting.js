8622d896798f34b8a11080f366100ab5f0a43103
pk4727 <pradhumanpk2018@gmail.com>
2025-08-25 22:04:47 +0530

    longestCommonPrefix

--- basic/sorting.js
+++ basic/sorting.js
@@ -885,3 +885,39 @@ Object.values(obj).sort()              → Sort values
 Object.entries(obj).sort(([,a],[,b]))  → Sort by value
 
 */
+
+
+
+// longest comman prifix ( matchAll(regex) → Iterator of all matches )
+
+let strArray = ["interstellar", "internet", "internal", "into"]; // "int"
+strArray = ["apple", "banana", "carrot"]; // ""
+strArray = ["hello", "hello", "hello"]; // "hello"
+strArray = ["cat", "caterpillar", "catch", "cattle"]; //"cat"
+
+function longestCommonPrefix(strs) {
+    if (!strs.length) return "";
+    let prefix = strs[0]; // start with first word
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1); // shrink prefix
+            if (prefix === "") return "";
+        }
+    }
+    return prefix;
+}
+console.log(longestCommonPrefix(strArray)); //"trans"
+
+
+function longestCommonPrefix2(strs) {
+    if (!strs.length) return "";
+    strs.sort();
+    let first = strs[0]
+    let last = strs[strs.length - 1];
+    let i = 0;
+    while (i < first.length && first[i] === last[i]) {
+        i++;
+    }
+    return first.slice(0, i);
+}
+console.log(longestCommonPrefix2(strArray)); // "fl"
